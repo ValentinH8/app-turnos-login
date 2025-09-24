eeb187e87da776ff118dc9ce3bf2359efb53d540
Valentin <valentinhernandezmateo@gmail.com>
2025-09-23 22:24:28 -0300

    primer commit

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -2,9 +2,9 @@ import 'dotenv/config';
 
 export const envs = {
   PORT: process.env.PORT ? parseInt(process.env.PORT) : 3001,
-  DB_USER: process.env.DB_USER || 'postgres',
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  DB_NAME: process.env.DB_NAME || 'turnos_db',
-  DB_PASSWORD: process.env.DB_PASSWORD || '!"#valentin',
+  DB_USER: process.env.DB_USER || 'sbuser',
+  DB_HOST: process.env.DB_HOST || 'dbhost',
+  DB_NAME: process.env.DB_NAME || 'dbname',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'password',
   DB_PORT: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432
 };
